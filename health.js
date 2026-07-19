42b1b78a2d8d97ee81d8976ecfea1d2d65c8c37c
Medsekma <medsekmasn@gmail.com>
2026-07-19 22:12:56 +0100

    Delete health.js

--- health.js
+++
@@ -1,6 +0,0 @@
-import { json } from './_shared.js';
-
-export async function onRequestGet({ env }) {
-  const configured = Boolean(env.GEMINI_API_KEY);
-  return json({ ok: true, version: 'cloudflare-v1', aiConfigured: configured, aiReady: configured, emailReady: false, leadStorage: 'Google Sheets' });
-}
